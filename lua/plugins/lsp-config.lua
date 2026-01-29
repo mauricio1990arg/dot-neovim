b06861a9ee6ddabf645f4b33b82dbf3a4f4698fe
j0k3r <pdasriogallegos@gmail.com>
2026-01-29 02:23:37 -0300

    correccion del atajo k

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -18,7 +18,6 @@ return {
         vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
         vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
         vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-        vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
         vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
         vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
         vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
